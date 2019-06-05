316ff7fb449fee3f934bdddb03543f6d1416f7f0
Vaidotas Gružas <vaidotas.gruzas@stud.viko.lt>
2019-06-05 22:03:14 +0300

    yet no sorting

---
+++ Voting_blockchain/P2P/IVoting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace P2P
+{
+    [ServiceContract(CallbackContract = typeof(IVoting))]
+    public interface IVoting
+    {
+        [OperationContract(IsOneWay = true)]
+        void Vote(Block block);
+    }
+}
---
+++ Voting_blockchain/P2P/VotingImplementation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace P2P
+{
+    public class VotingImplementation : IVoting
+    {
+        Blockchain bc = new Blockchain();
+        int naus = 0;
+        int sim = 0;
+        int skve = 0;
+        public void Vote(Block block)
+        {
+            if(bc.IsValid())
+                bc.AddBlock(block);
+
+            if (block.GetData() == "1")
+                sim += 1;
+            if (block.GetData() == "2")
+                naus += 1;
+            if (block.GetData() == "3")
+                skve += 1;
+
+            Console.WriteLine("Nauseda: " + naus);
+            Console.WriteLine("Simonyte: " + sim);
+            Console.WriteLine("Skvernelis: " + skve);
+            Console.WriteLine("---------------");
+            Console.WriteLine("1. Simonyte");
+            Console.WriteLine("2. Nauseda");
+            Console.WriteLine("3. Skvernelis");
+            Console.WriteLine("---------------");
+            Console.Write(">");
+        }
+    }
+}
