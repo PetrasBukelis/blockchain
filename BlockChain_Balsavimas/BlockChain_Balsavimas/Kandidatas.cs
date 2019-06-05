2491eee80707c1669c1806aa2a0f1a2510b55997
PetrasBukelis <34337376+PetrasBukelis@users.noreply.github.com>
2019-06-05 13:47:26 +0300

    Kandidatas class + fixed project

---
+++ BlockChain_Balsavimas/BlockChain_Balsavimas/Kandidatas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain_Balsavimas
+{
+    class Kandidatas
+    {
+        private string name;
+        private string img;
+        private int votes;
+
+        public Kandidatas(string name, string img)
+        {
+            this.name = name;
+            this.img = img;
+            votes = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetImg()
+        {
+            return img;
+        }
+
+        public int GetVotes()
+        {
+            return votes;
+        }
+
+        public void AddVote()
+        {
+            votes += 1;
+        }
+
+        public void SetVotesToZero()
+        {
+            votes = 0;
+        }
+    }
+}
